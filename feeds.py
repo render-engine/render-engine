12463d262d76fa51a4848ba5e48fa16520a71513
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-03-05 13:11:56 -0800

    manual add for output_path and name

--- feeds.py
+++ feeds.py
@@ -1,8 +1,11 @@
 import json
 
-def feed_gen(collection):
-    with open(f'{collection.output_path}/{collection.name}.json', 'w') as fp:
+def feed_gen(collection, output_path=None, name=None):
+    output_path = output_path or collection.output_path
+    name = name or collection.name
+
+    with open(f'{output_path}/{name}.json', 'w') as fp:
         json.dump(collection.json_feed, fp)
 
-    with open(f'{collection.output_path}/{collection.name}.rss', 'w') as rss:
+    with open(f'{output_path}/{name}.rss', 'w') as rss:
         rss.write(collection.rss_feed)
