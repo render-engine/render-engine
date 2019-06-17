ed82ae2c4720f7aaf435815acbeb14aeb18b16cd
kmiller <kjaymiller@gmail.com>
2019-06-17 14:16:20 -0700

    create Engine Object

--- __init__.py
+++ __init__.py
@@ -3,4 +3,10 @@ from .blog import BlogPost
 from .microblog import MicroBlogPost
 from .collection import Collection
 from .environment import env
-from .run import run
+
+class Engine:
+    def run(overwrite=True):
+        return gen_static(
+                static_path=STATIC_PATH,
+                overwrite=overwrite
+                )
