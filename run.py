fb87634ecfd46af0a3432fe4f28e2da56a8a4b76
kmiller <kjaymiller@gmail.com>
2019-06-17 14:04:15 -0700

    import static path

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from generators import gen_static
+from config import STATIC_PATH
 
 def run(overwrite=True):
-    return gen_static(static_path=config.STATIC_PATH, overwrite=overwrite)
+    return gen_static(static_path=STATIC_PATH, overwrite=overwrite)
