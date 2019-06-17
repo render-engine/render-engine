b5081ffd5d322cb5d502250476a6b9b7ac2f46fa
kmiller <kjaymiller@gmail.com>
2019-06-17 14:24:35 -0700

    create Engine Object

--- __init__.py
+++ __init__.py
@@ -5,7 +5,16 @@ from .collection import Collection
 from .environment import env
 
 class Engine:
+    def __init__(self, collections):
+        self.collections = collections
+
     def run(overwrite=True):
+        for collection in self.collections:
+            collection.output_path.mkdir(parents=True, exist_ok=True)
+
+            for page in collection.pages:
+                write_page(f'{collection.output_path}/{page.id}.html', page.html)
+
         return gen_static(
                 static_path=STATIC_PATH,
                 overwrite=overwrite
