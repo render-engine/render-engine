281fa04b738240cd2e9a1d1825945b9674f4bf46
kjaymiller <kjaymiller@gmail.com>
2019-06-17 17:52:48 -0700

    buidling out collections in engine

--- engine.py
+++ engine.py
@@ -2,36 +2,31 @@ from generators import gen_static
 from base_config import config
 
 class Engine:
-    """This is the engine that is builds your static site. 
-    
-    Use Engine.run() to output the files to the designated output path."""
-    
+    """This is the engine that is builds your static site.
+    Use `Engine.run()` to output the files to the designated output path."""
+
+    # Currently all of the Configuration Information is saved to Default
     config = config['DEFAULT']
-    
+
     def __init__(self, *, conent_path, output_path):
         self.conent_path = content_path
         self.output_path = output_path
-        
+
     def build(self, collections):
         static_pages = gen_static(
             static_path=STATIC_PATH,
             overwrite=overwrite,
             )
-            
-        page_collections = pages, blog
 
-        for collection in page_collections:
+        for collection in collections:
             collection.output_path.mkdir(
-            parents=True, 
+            parents=True,
             exist_ok=True,
             )
-        
-        
+
     def run(self, overwrite=True):
-        for page in collection.pages:
+        for page in self.collections:
             write_page(
-            f'{collection.output_path}/{page.id}.html', 
+            f'{collection.output_path}/{page.id}.html',
         page.html)
 
-    
-    return 
