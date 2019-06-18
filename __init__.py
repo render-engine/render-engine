0330075da1d04310b28328a8793d2a9503ce2344
kmiller <kjaymiller@gmail.com>
2019-06-18 15:31:48 -0700

    remove config call

--- __init__.py
+++ __init__.py
@@ -3,6 +3,3 @@ from .blog import BlogPost
 from .microblog import MicroBlogPost
 from .collection import Collection
 from .environment import env
-import config
-
-with open('./base_config', config)
