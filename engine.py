78775abdff55d0e7d12e8e56d267f935b4edd921
kmiller <kjaymiller@gmail.com>
2019-06-25 13:59:48 -0700

    add_route and add_collection

--- engine.py
+++ engine.py
@@ -5,6 +5,21 @@ import shutil
 # Currently all of the Configuration Information is saved to Default
 config = config['DEFAULT']
 
+def paginate(iterable, items_per_page, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * items_per_page
+    iterable = zip_longest(*args, fillvalue=fillvalue)
+    return iterable
+
+
+def write_paginated_pages(name, pagination, template, path, **kwargs):
+    temp =  env.get_template(template)
+    for block in enumerate(pagination):
+        render = temp.render(post_list=[b for b in block[1] if b], config=config, **kwargs)
+        write_page(f'{path}/{name}_{block[0]}.html', render)
+
+
 class Engine:
     """This is the engine that is builds your static site.
     Use `Engine.run()` to output the files to the designated output path."""
@@ -28,21 +43,63 @@ class Engine:
         except:
             pass
 
-    def build(self, content_type, *, template, route):
+    def add_route(self,
+            content_type,
+            *,
+            route,
+            template,
+            base_file==None,
+            **kwargs,
+            ):
+        """Used to Create the HTML that will be added to Routes. Usually not
+        called on it's own."""
+
+        content = content_type(
+                template=template,
+                base_file=base_file,
+                **kwargs,
+                ).html
+
+        self.routes[f'{route}.html'] = content
+        return content
+
+    def build(self, content_type, *, template, route, base_file=None):
+        """Used to get **kwargs for `add_route`"""
+
         def inner(func):
             kwargs = func() or {}
-            content = content_type(template=template, **kwargs).html
-            self.routes[f'{route}.html'] = content
+            self.add_route(
+                    content_type,
+                    route=route,
+                    template=template,
+                    **kwargs
+                    )
             return func
 
         return inner
 
-    def collection(self, paginate=True, feed=True, feed_template=None):
-        def inner(func):
-            return funct
+    def add_collection(
+            self,
+            *,
+            content_path,
+            template,
+            output_path,
+            paginate=True,
+            feed=True,
+            feed_template=None,
+            extenstion='.md',
+            ):
+        """Iterate through the provided content path building the desired
+        content_type and storing in routes to be created on run"""
 
-        return inner
-
+        for path in content_path.glob(f'*{extension}'):
+            self.add_route(
+                    content_type,
+                    route=route,
+                    template=template,
+                    base_file=path,
+                    **kwargs,
+                    )
 
     def run(self, overwrite=True):
         """Builds the Site Objects
@@ -58,22 +115,24 @@ class Engine:
             f"{config['OUTPUT_PATH']}/{config['STATIC_PATH']}",
             )
 
-        for collection in self.collections:
-            Path(config['OUTPUT_PATH']).joinpath(
-                    collection.output_path).mkdir(
-                    parents=True,
-                    exist_ok=True,
-                    )
-
             for page in collection.pages:
                 route = f'{collection.output_path}/{page.id}.html'
                 content = page.html
                 self.routes[route] = content
 
 
-        for page in self.routes:
-            filename = f'{self.output_path}/{page}'
-            with open(filename, 'w') as f:
-                print(filename + ' has been added')
-                f.write(self.routes[page])
+        for path, content in self.routes.items():
+            filename = Path(path).resolve()
+            base_dir = filename.parent.mkdir(
+                    parents=True,
+                    exist_ok=True,
+                    )
+
+            if filename.exists():
+                with filename.open() as f:
+                    if f.read() == content:
+                        continue
+
+            with filename.open('w') as f:
+                f.write(content)
 
