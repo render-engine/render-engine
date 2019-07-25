26377c0793c177c026fbfa6199979c1e30001dad
kmiller <kjaymiller@gmail.com>
2019-07-25 10:25:32 -0700

    update page

--- page.py
+++ page.py
@@ -7,15 +7,9 @@ from .utils import git_log_date
 import re
 import maya
 
-def get_ct_time(md_file):
-    return maya.when(git_log_date(md_file, 'head'))
-
-def get_md_time(md_file):
-    return maya.when(git_log_date(md_file, 'tail'))
-
-def load_from_file(base_file):
+def load_from_file(content_path):
     matcher = r'^\w+:'
-    _ = base_file.read_text()
+    _ = content_path.read_text()
     md_content = _.splitlines()
     attrs = {}
 
@@ -34,91 +28,44 @@ def load_from_file(base_file):
 
 class Page():
     env = env
+
     def __init__(
             self,
             *,
             output_path,
-            base_file=None,
+            content_path=None,
             content='',
             content_format='markdown',
-            **kwargs,
+            **attrs,
             ):
-        # self.id looks for us
-        self._id = None
-        self._slug = None
-        self._date_published = None
-        self._date_modified = None
-        self._category = 'Uncategorized'
-        self._image = None
-        self.summary = None
-        self.attrs = kwargs.copy()
+        self.content_path = Path(content_path)
 
-        if base_file:
-            _ = load_from_file(base_file)
-            self.attrs.update(_['attrs'])
+        if content_path:
+            _ = load_from_file(content_path)
+            attrs.update(_['attrs'])
             self.content = _['content']
 
-        else:
-            self.content = content
-
-        # self.date_published looks for us
-        self._date = None
-        self._updated = None
-
-        self.base_file = base_file
-
-        self.title = getattr(self, '_title', '')
-        self.date_published = self.get_date_published()
-        self.date_modified = self.get_date_modified()
         self.markup = markdown(self.content)
 
-    @property
-    def id(self):
-        base_file_stem = self.base_file.stem if self.base_file else None
-        return self._id or self._slug or base_file_stem or ''
+        if 'date_published' in attrs:
+            date_published = attrs['date_published']
+        elif self.content_path:
+            date_published = git_log_date(self.content_path)[-1]
+        self.date_published = maya.when(date_published).iso8601()
 
+        if 'date_modified' in attrs:
+            date_modified = attrs['date_modified']
+        elif self.content_path:
+            date_modified = git_log_date(self.content_path)[0]
+        self.date_modified = maya.when(date_modified).iso8601()
 
-    def get_date_published(self):
-        """Returns the value of _date_published or _date, or created_datetime from
-the system if not defined. NOTE THE SYSTEM DATE IS KNOWN TO CAUSE
-ISSUES WITH FILES THAT WERE COPIED OR TRANSFERRED WITHOUT THEIR
-METADATA BEING TRANSFER READ AS WELL"""
+        if not 'id' in attrs:
+            if 'slug' in attrs:
+                self.id = attrs['slug']
+            elif 'content_path':
+                self.id = self.content_path.stem
 
-        if self.base_file:
-
-            if self._date_published:
-                date = maya.when(self._date_published)
-
-            elif self._date:
-                date = maya.when(self._date)
-
-            else:
-                 date = get_ct_time(self.base_file)
-
-            return date
-
-    def get_date_modified(self):
-        """Returns the value of _date_modified or _update, or the
-modified_datetime from the system if not defined. NOTE THE SYSTEM 
-DATE IS KNOWN TO CAUSE ISSUES WITH FILES THAT WERE COPIED OR 
-TRANSFERRED WITHOUT THEIR METADADTA BEING TRANSFERRED AS WELL"""
-
-        if self.base_file:
-            print(self.base_file)
-
-            if self._date_modified:
-                print(self._date_modified)
-                date = maya.when(self._date_modified)
-
-            elif self._updated:
-                date = maya.when(self._date)
-
-            else:
-                date = get_md_time(self.base_file)
-
-            return date
-
-    @property
-    def image(self):
-        return self._image
+        # make properties for all attrs
+        for key, attr in attrs.items():
+            setattr(self, key, attr)
 
