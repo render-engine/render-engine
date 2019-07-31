fb44fefb50ab7eee71b501fbb61d4cf7f61df29b
kjaymiller <kjaymiller@gmail.com>
2019-07-30 17:25:02 -0700

    update post-list

--- paginate.py
+++ paginate.py
@@ -6,7 +6,6 @@ def write_paginated_pages(name, pagination, *, route, content_type, **kwargs):
             page = content_type(
                     template=kwargs.get('template', 'archive.html'),
                     route=block_route,
-                    post_list=list(enumerate([x for x in list(filter(lambda x:x, block[1]))])),
                     post_list=post_list,
                     )
             p_routes.append(page)
