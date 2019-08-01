8304c601a34aba6515a82a33c0757a2184aae33c
kmiller <kjaymiller@gmail.com>
2019-08-01 15:21:45 -0700

    tests for file_upload

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2019.8.1dev0',  # Required
+    version='2019.8.1dev1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
