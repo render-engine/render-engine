c4930872f3a2c60cb96eae40cbaa22dc5a07ff02
kmiller <kjaymiller@gmail.com>
2019-08-27 08:31:55 -0700

    remove other tests

--- tests/unit_tests/test_collection.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def test_collection_kwargs_become_properties(base_collection):
-    assert base_collection.custom_val == 'custom'
-
-def test_collection_sorts_alphabetically(base_collection):
-    assert base_collection.pages[0].slug == 'Title_C'
