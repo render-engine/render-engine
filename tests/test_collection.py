46c1c6dd5cb6fcac3db52a96d6164dafa1f21bf5
kmiller <kjaymiller@gmail.com>
2019-08-29 15:59:30 -0700

    remove index call

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -5,6 +5,13 @@ import pytest
 def base_collection():
     return Collection()
 
-def test_collection_(base_collection):
+def test_collection(base_collection):
     assert not base_collection.template
     assert not base_collection.template_vars
+
+@pytest.mark.parametrize('collection, template_var', [
+    (Collection(template='foo_path.html'), {}),
+    (Collection(), None)],
+    )
+def test_collection_if_template_then_vars(collection, template_var):
+    assert getattr(collection, 'template_var', None) is None
