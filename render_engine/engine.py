f2bdbac2454744684b8a4e8d918505ed9e2471c0
kmiller <kjaymiller@gmail.com>
2019-08-30 13:30:55 -0700

    initial upload of engine story

--- render_engine/engine.py
+++ render_engine/engine.py
@@ -8,10 +8,9 @@ from dataclasses import dataclass
 from itertools import zip_longest
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
-from typing import Type, Optional, Union, TypeVar, Iterable
+from typing import Type, Optional, Union, TypeVar
 
 import logging
-import json
 import os
 import shutil
 import yaml
@@ -24,160 +23,28 @@ PathString = Union[str, Type[Path]]
 class Engine:
     """This is the engine that is builds your static site.
     Use `Engine.run()` to output the files to the designated output path."""
-    def __init__(
-            self,
-            *,
-            site_url=None,
-            template_path='./templates',
-            config={},
-            config_path=None,
-            content_path='content',
-            static_path='static',
-            output_path='output',
-            pages=[],
-            collections=[],
-            **kwargs,
+
+    def __init__(self,
+            output_path:PathString=Path('output'),
+            static_path:PathString=Path('static'),
+            strict: bool=False,
+            env_variables: dict:{},
+            templates_dir: PathString=Path('templates'),
             ):
 
-        # Check for configurations in config_path or kwargs
-        if config_path:
-            logging.info(f'{config_path} detected checking for engine variables')
-            config.update(load_config(config_path, 'Engine'))
-            logging.debug(f'config={config}')
 
-        if 'Environment' not in config:
-            config['Environment'] = {}
+        self.output_path = Path(output_path)
+        if not self.output_path.is_dir():
 
-        config['Environment'].update(kwargs)
+            if strict:
+                shutil.rmtree(self.output_path)
 
+            self.output_path.mkdir(exist_ok=True)
 
-        # Create a new environment and set the global variables to the config
-        # items called in environment variables
-        self.env = Environment(
-            loader=FileSystemLoader(template_path),
-            autoescape=select_autoescape(['html', 'xml']),
-            )
+        self.static_path = Path(static_path)
+        if self.static_path.is_dir():
+            output_static_path = self.output_path.joinpath(self.static_path)
 
-        if 'Environment' in config:
-            logging.info(f'Environment section detected')
-            logging.debug(config['Environment'])
-            self.env.globals.update(config['Environment'])
-
-        # These fields are called a lot. So we pull them from config. Also,
-        # make it a path
-        self.base_content_path = directory_path(
-                config.get('content_path', content_path))
-        self.base_static_path = directory_path(
-                config.get('static_path', static_path))
-        self.base_output_path= directory_path(
-                config.get('output_path', output_path))
-
-        self.site_url = config.get('site_url', site_url)
-        self.pages = pages
-        self.collections = collections
-        logging.debug(f'base_content_path - {self.base_content_path}')
-        logging.debug(f'base_output_path - {self.base_output_path}')
-        logging.debug(f'base_static_path - {self.base_static_path}')
-        logging.debug(f'site_url - {self.site_url}')
-        logging.debug(f'pages - {self.pages}')
-        logging.debug(f'collections - {self.collections}')
-
-    def route(self, *routes, content_path=None, content=None, template=None, content_type=Page):
-        """Used to get **kwargs for `add_route`"""
-        def inner(func, routes=routes, content_path=content_path,):
-            kwargs = func() or {}
-
-            for route in routes:
-                self.routes.append(
-                        content_type(
-                            content_path=content_path,
-                            content=content,
-                            template=template,
-                            slug=route.lstrip('/'),
-                            **kwargs,
-                            )
-                        )
-
-            return func
-
-        return inner
-
-    def collection(
-            self,
-            *routes,
-            pages=None,
-            template='page.html',
-            content_path=None,
-            name=None,
-            content_type=Page,
-            **kwargs,
-            ):
-        """creates a collection of objects"""
-        def inner(func, routes=routes, content_path=content_path,):
-            kwargs = func() or {}
-
-            for route in routes:
-                collection = Collection(
-                        name=name,
-                        content_path=Path(self.base_content_path) \
-                                .joinpath(content_path \
-                                if content_path else './'),
-                        pages=pages,
-                        route=route,
-                        extension=extension,
-                        template=template,
-                        **kwargs,
-                        )
-
-                self.routes.extend(iter(collection))
-
-    def run(self, overwrite=True):
-        """Builds the Site Objects
-        1. Generate Static Pages
-        2. Generate Collections
-        3. Generate Custom Pages
-
-        TODO: Add Skips to ByPass Certain Steps
-        """
-        static_output = f"{self.base_output_path}/{self.base_static_path}"
-
-        # If overwrite AND THE FILE EXISTS, then remove the entire folder
-        if all((overwrite, Path(self.base_output_path).exists())):
-            shutil.rmtree(self.base_output_path)
-            Path(self.base_output_path).mkdir() # Creates the new folder
-
-        # Instead of trying to analyze the static folder. Just delete the
-        # contents
-        try:
-            shutil.rmtree(static_output)
-            shutil.copytree(self.base_static_path, static_output)
-
-        except:
-            pass
-
-
-        for route in self.routes:
-            # Get filename from route
-            filename = Path(self.base_output_path +
-                    str(route.url).split(self.base_content_path)[-1])
-            base_dir = Path(filename).parent.mkdir(
-                    parents=True,
-                    exist_ok=True,
-                    )
-
-            if filename.exists():
-                if filename.read_text == route.content:
-                    continue
-
-                else:
-                    filename.unlink()
-
-            if route.template:
-                template = self.env.get_template(route.template)
-                params = dict(route.__dict__)
-                content = template.render(**params)
-
-            else:
-                content = route.content
-
-            filename.write_text(content)
+            if output_static_path.exists():
+                shutil.rmtree(output_static_path)
+                shutil.copytree(src=static_path, dst=output_static_path)
