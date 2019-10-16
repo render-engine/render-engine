972a06da7b4d0d533a969b68c25b8135f6b6b1c7
kmiller <kjaymiller@gmail.com>
2019-10-16 16:01:50 -0700

    remove old files

--- render_engine/category_feeds.py
+++
@@ -1,5 +0,0 @@
-    title = _[1].capitalize()
-    with open(f'{_[0]}/{_[1]}_feed.json', 'w') as f:
-        json.dump(json_feed(items=_[0]['pages'],
-                path=output_path,
-                title=), _[0]['output_path'])
