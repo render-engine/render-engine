a1056702a860167ac1e4414c4f2c42d9e5c6c9a5
kmiller <kjaymiller@gmail.com>
2019-10-30 16:11:19 -0700

    add rss items

--- render_engine/feeds.py
+++ render_engine/feeds.py
@@ -7,7 +7,7 @@ standards defined.
 RSS: http://www.rssboard.org/rss-specification
 JSON: https://jsonfeed.org/version/1
 """
-
+from more_itertools import first_true
 from .page import Page
 
 
@@ -15,35 +15,17 @@ class RSSFeedItem():
     version = 2.0
 
     def __init__(self):
-        if getattr(self, 'title', None) == True:
-            self.title = ''
+        title = getattr(self, 'title', '')
+        description = getattr(self, 'description', None)
+        content = getattr(self, 'content', None)
+        summary = getattr(self, 'summary', None)
+        self.description = first_true([description, content, summary], None)
 
-        if getattr(self, 'description', None) == True:
-            pass
-
-        elif getattr(self, 'content', None) == True :
-            self.description = self.content
-
-        elif getattr(self, 'summary', None) == True:
-            self.description = self.summary
-
-        else:
-            Page.description = ''
-
-        if not any([self.title, self.description]):
+        if not self.title and not self.description:
             error_msg = "Your page must have either a title or a description"
             raise AttributeError(error_msg)
 
-        if getattr(self, 'guid', None) == True:
-            pass
-
-        else:
-            self.guid = self._slug
-
-
-    @property
-    def content(self):
-        pass
+        self.guid = getattr(self, 'guid', None) or self._slug
 
 
 class RSSFeed(Page):
