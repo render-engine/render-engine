763cace735a70d164d1350f748c6db6f6ae714ae
kmiller <kjaymiller@gmail.com>
2020-02-19 16:01:03 -0800

    update conf.py with sphinx extensions

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ author = 'Jay Miller'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.autodoc',
+        'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
