698a01ebc9f4a91469b15dbe2ff58bf4043d09bf
Jay Miller <kjaymiller@gmail.com>
2022-03-24 21:39:32 -0700

    updated links option

--- render_engine/links.py
+++ render_engine/links.py
@@ -1,5 +1,6 @@
 import typing
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 
 
 @dataclass
@@ -12,19 +13,61 @@ class Link:
         myImage = Link(
             name="Render Engine",
             url="https://render-engine.site"
+            alt="example image"
             )
 
 
     In this case the name and url give you enough information that you can supply to your template::
 
-        <a href="{{myImage.url}}">{{myImage.name}}</a>
+        <a href="{{myImage.url}}">{{myImage.text}}</a>
 
     Any attribute could be applied but here are some that you can supply on initialization.
     """
 
-    name: str
-    url: str
-    links: typing.Optional[typing.List[typing.Any]] = None
-    image: str = ""
-    icon: str = ""
-    alt: str = ""
+    text: str = field(kw_only=True, default_factory=str)
+    url: str = field(kw_only=True, default="#")
+    meta: dict[str, str] = field(default_factory=dict)
+    
+    def __str__(self):
+        """href is the common """
+
+        if self.meta:
+            attrs = " ".join([f'{k}="{v}"' for k,v in self.meta.items()])
+            starting_path = f'<a href="{self.url}" {attrs}>'
+        
+        else:
+            starting_path = f'<a href="{self.url}">'
+
+        return f'{starting_path}{self.text}</a>'
+        
+
+@dataclass
+class Image(Link):
+    """
+    Link object formatted as an image
+
+    Look at the example::
+
+        myImage = Link(
+            name="Render Engine",
+            url="https://render-engine.site"
+            alt="example image"
+            )
+
+
+    In this case the name and url give you enough information that you can supply to your template::
+
+        <a href="{{myImage.url}}">{{myImage.text}}</a>
+
+    Any attribute could be applied but here are some that you can supply on initialization.
+    """
+
+    def __str__(self):
+        """prints the image point"""
+
+        if self.meta:
+            attrs = " ".join([f'{k}="{v}"' for k,v in self.meta.items()])
+            return f'<img src="{self.url}" alt="{self.text}" {attrs} />'
+        
+        else:
+            return f'<img src="{self.url}" alt="{self.text}" />'
---
+++ tests/test_links.py
@@ -0,0 +1,33 @@
+import pytest
+from render_engine.links import Link, Image
+
+
+def test_links_values():
+    """
+    given a link object.
+    tests that class and id don't need to be included.
+    """
+
+    link_no_options = Link()
+    assert str(link_no_options) == '<a href="#"></a>'
+
+    link_with_options = Link(
+        text="test item",
+        meta={"foo": "bar"}
+        )
+
+    assert str(link_with_options) == '<a href="#" foo="bar">test item</a>'
+
+
+def test_images():
+    """
+    given a link object.
+    tests that class and id don't need to be included.
+    """
+
+    image_with_meta = Image(
+        text="test item",
+        meta={"foo": "bar"}
+        )
+
+    assert str(image_with_meta) == '<img src="#" alt="test item" foo="bar" />'
