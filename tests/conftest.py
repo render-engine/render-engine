5c08f445f0bdff819750243b16c9ece79704f103
Jay Miller <kjaymiller@gmail.com>
2022-04-06 22:03:14 -0700

    update tests sessions

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,9 @@ import pytest
 from render_engine import Page
 from jinja2 import Template
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def content():
-   return """
+   yield """
 ---   
 title: Test Title
 custom_list: foo, bar, biz
@@ -18,15 +18,20 @@ Test Paragraph
 
 `<p>Raw HTML</p>`"""
 
-@pytest.fixture()
+
+@pytest.fixture(scope='session')
+def temp_dir(tmpdir_factory):
+    yield tmpdir_factory.mktemp('test_dir')
+
+@pytest.fixture(scope='class', name='page')
 def base_page():
     """Tests can a simple Page be created given no Parameters"""
     class BasePage(Page):
         pass
 
-    return BasePage()
+    yield BasePage()
 
-@pytest.fixture()
+@pytest.fixture(scope='module', name='p_attrs')
 def page_with_attrs():
     """Supply this page objects with attributes"""
 
@@ -38,34 +43,30 @@ Test Paragraph"""
         custom_list = ['foo', 'bar', 'biz']
         custom_attr = "this is an attribute"
 
-    return PageWithAttrs()
-
-@pytest.fixture()
-def base_collection():
-    return Collection
+    yield PageWithAttrs()
 
 
-@pytest.fixture()
-def page_with_content_path(tmp_path, content):
-    fake_path = tmp_path / 'fake_path.md'
+@pytest.fixture(scope='class', name='with_path')
+def page_with_content_path(temp_dir, content):
+    fake_path = Path(temp_dir / 'fake_path.md')
     fake_path.write_text(content)
 
     class PageWithContentPath(Page):
         content_path = fake_path
         list_attrs = 'custom_list'
 
-    return PageWithContentPath()
+    yield PageWithContentPath()
 
 
-@pytest.fixture()
-def render_page_no_template(tmp_path, page_with_attrs):
-        page_with_attrs.render(output_path = tmp_path)
-        check_path =  Path(tmp_path / f"{page_with_attrs.slug}{page_with_attrs.extension}")
-        return check_path
+@pytest.fixture(scope='class', name='no_template')
+def render_page_no_template(temp_dir, p_attrs):
+        p_attrs.render(output_path = temp_dir)
+        check_path =  Path(temp_dir / f"{p_attrs.slug}{p_attrs.extension}")
+        yield check_path
     
 
-@pytest.fixture()
-def render_page_template(tmp_path, page_with_attrs):
-        page_with_attrs.render(output_path = tmp_path, template=Template('foo{{content}}'))
-        check_path =  Path(tmp_path / f"{page_with_attrs.slug}{page_with_attrs.extension}")
-        return check_path
+@pytest.fixture(scope='class', name='with_template')
+def render_page_template(temp_dir, p_attrs):
+        p_attrs.render(output_path = temp_dir, template=Template('foo{{content}}'))
+        check_path =  Path(temp_dir / f"{p_attrs.slug}{p_attrs.extension}")
+        yield check_path
--- tests/test_page.py
+++ tests/test_page.py
@@ -10,74 +10,76 @@ logger.setLevel(logging.INFO)
 
 
 class TestPageWithContentPath:
-    def test_page_defines_attrs_and_content_from_frontmatter(self, page_with_content_path):
+    def test_page_defines_attrs_and_content_from_frontmatter(self, with_path):
         """
         Tests when given a file as the content_path, parse it into data.
         This is done using fronmatter and should pass as long as frontmatter is parser
         """
 
-        assert page_with_content_path.markdown =="""# Test Header
+        assert with_path.markdown =="""# Test Header
 
 Test Paragraph
 
 `<p>Raw HTML</p>`"""
 
-        assert page_with_content_path.title == 'Test Title'
+        assert with_path.title == 'Test Title'
 
-    def test_page_content_converts_markdown_to_html(self, page_with_content_path):
+    def test_page_content_converts_markdown_to_html(self, with_path):
         """Tests that `Page.html` is converted to html"""
 
-        assert page_with_content_path.content == """<h1>Test Header</h1>
+        assert with_path.content == """<h1>Test Header</h1>
 
 <p>Test Paragraph</p>
 
 <p><code>&lt;p&gt;Raw HTML&lt;/p&gt;</code></p>
 """
 
-    def test_page_with_content_path(self, page_with_content_path):
-        assert page_with_content_path.custom_list == ['foo', 'bar', 'biz']
+    def test_with_path(self, with_path):
+        assert with_path.custom_list == ['foo', 'bar', 'biz']
 
 
-class TestPageWithAttrs:
-    def test_page_with_attrs_gets_attrs_from_class(self, page_with_attrs):
-        assert page_with_attrs.title == "Page with Attrs"
+class TestPageWithAttrs():
+    def test_page_with_attrs_gets_attrs_from_class(self, p_attrs):
+        assert p_attrs.title == "Page with Attrs"
 
-    def test_page_slug_is_slugified(self, page_with_attrs):
+    def test_page_slug_is_slugified(self, p_attrs):
         """Test page text is slugified"""
-        assert page_with_attrs.slug == 'page-slug-with-attrs'
+        assert p_attrs.slug == 'page-slug-with-attrs'
 
 
-    def test_base_page_html_with_content_is_converted_from_markdown(self, page_with_attrs):
+    def test_base_page_html_with_content_is_converted_from_markdown(self, p_attrs):
         """If there is no content then the html will be None"""
 
-        assert page_with_attrs.markdown == '# Test Header\nTest Paragraph'
+        assert p_attrs.markdown == '# Test Header\nTest Paragraph'
 
 
 class TestBasePage:
-    def test_base_page_is_slug(self, base_page):
+    def test_base_page_is_slug(self, page):
         """Tests if a slug is not provided then the slug will be a slugified
         version of the the class name"""
-        assert base_page.slug == 'basepage'
+        assert page.slug == 'basepage'
+        assert str(page) == 'basepage'
+        assert page.url == './basepage'
 
 
-    def test_page_html_with_no_content_is_empty_string(self, base_page):
+    def test_page_html_with_no_content_is_empty_string(self, page):
         """If there is no content then the html will be None"""
         
-        assert base_page.markdown == None
+        assert page.markdown == None
         
         with pytest.raises(ValueError) as e:
-            base_page.content
+            page.content
 
 
 
 class TestPageWritesToFile():
     
-    def test_page_writes_to_file(self, render_page_no_template):
+    def test_page_writes_to_file(self, no_template):
         """Given a Path with """
-        assert render_page_no_template.exists()
+        assert no_template.exists()
         
-    def test_page_content_no_template_is_html(self, render_page_no_template, page_with_attrs):
-        assert render_page_no_template.read_text() == page_with_attrs.content
+    def test_page_content_no_template_is_html(self, no_template, p_attrs):
+        assert no_template.read_text() == p_attrs.content
 
-    def test_page_with_tempate_is_rendered(self, render_page_template, page_with_attrs):
-        assert render_page_template.read_text() == f'foo{page_with_attrs.content}'
+    def test_page_with_tempate_is_rendered(self, with_template, p_attrs):
+        assert with_template.read_text() == f'foo{p_attrs.content}'
