e47809a80c6c63d04690efa2a7829bc046c885b6
Jon Banafato <jon@jonafato.com>
2022-09-04 21:24:04 -0400

    Fix test_collection_render_archives_loaded
    `base_collection.render_archives` was removed in
    752356658f054330aa4f3c735029257514fb0c5e. This method's functionality
    seems to have moved to `base_collection.archives` (a decorator that
    generates the archives), `gen_collection` (a function that powers that
    decorator), and `render_archives` (a function that does the actual
    writing of the flat files). This change updates the relevant test to
    resolve its failure.
    
    Additionally, f628b1833b4b846b0c59121a0073df023a42c780 removed `engine`
    from pages and collections, so this test now includes an engine passed
    explicitly into the `render_archives` call.

--- tests/test_collection/test_collection.py
+++ tests/test_collection/test_collection.py
@@ -2,8 +2,11 @@ from pathlib import Path
 
 import pytest
 
-from render_engine.collection import Collection
+from jinja2 import Environment, FileSystemLoader
+
+from render_engine.collection import Collection, gen_collection
 from render_engine.page import Page
+from render_engine.site import render_archives
 
 
 def test_collections_discovers_title(base_collection, custom_collection):
@@ -42,9 +45,9 @@ def test_collection_with_bad_path_raises_error():
         BadPathCollection().pages()
 
 
-@pytest.mark.xfail(strict=True)
 def test_collection_render_archives_loaded(temp_dir_collection, base_collection):
-    base_collection.render_archives(path=temp_dir_collection)
+    engine = Environment(loader=FileSystemLoader(["templates"]))
+    render_archives(archive=base_collection.archives, engine=engine, path=temp_dir_collection)
     archive = temp_dir_collection.joinpath("mycollection.html")
     assert archive.exists()
     assert archive.read_text() == "Foo"
