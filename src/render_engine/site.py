0d59e71423ef27bc3f924f393a8bc3d70d63b58b
Jay Miller <kjaymiller@gmail.com>
2022-11-12 20:21:33 -0800

    remove rich formatting

--- src/render_engine/site.py
+++ src/render_engine/site.py
@@ -7,27 +7,12 @@ from pathlib import Path
 from re import sub
 
 from jinja2 import Environment, FileSystemLoader
-from rich import print as rprint
-from rich import progress
-from rich.padding import Padding
-from rich.panel import Panel
-from rich.pretty import pprint
-from rich.progress import Progress
 
 from .collection import Archive, Collection
 
 
 def render_archives(archive, **kwargs) -> list[Archive]:
     return [archive.render(pages=archive.pages, **kwargs) for archive in archive]
-
-
-p = Progress(
-    progress.SpinnerColumn(),
-    progress.TextColumn("|{task.description}"),
-    progress.BarColumn(),
-    progress.TimeElapsedColumn(),
-    progress.MofNCompleteColumn(),
-)
 
 
 class Site:
@@ -50,9 +35,6 @@ class Site:
 
     def __init__(self, static: typing.Optional[str] = None, **kwargs):
         self.site_vars.update(kwargs)
-        rprint(Panel(f"Rendering {self.__class__.__name__}"))
-        pad = Padding(f"Site Config Keys Imported: {[x for x in self.site_vars]}", 1)
-        rprint(pad)
 
         if static:
             self.render_static(directory=static)
@@ -71,23 +53,11 @@ class Site:
                 pass
         """
 
-        build_collection = p.add_task(
-            f"[deep_sky_blue3]Rendering {collection.__name__}", total=4
-        )
-
         _collection = collection()
         collection_path = self.path / _collection.output_path
-        creating_output_path = p.add_task(
-            f"[dark_olive_green1]- creating {collection_path=}", total=1
-        )
         collection_path.mkdir(exist_ok=True, parents=True)
-        p.update(creating_output_path, completed=1)
 
         page_count = len(_collection.pages)
-        rendering_pages = p.add_task(
-            f"[sky_blue1]- rendering {page_count} {collection.content_type.__name__} objects",
-            total=page_count,
-        )
         collection_vars = {
             f"collection_{key}".upper(): val for key, val in vars(collection).items()
         }
@@ -98,15 +68,8 @@ class Site:
                 engine=self.engine,
                 **self.site_vars,
             )
-            p.update(rendering_pages, advance=1)
-
-        p.update(build_collection, advance=1)
 
         if hasattr(_collection, "feed"):
-            create_feed = p.add_task(
-                f"[gold3]- creating {_collection.feed.__name__} for [sky_blue3]{collection.__name__}",
-                total=1,
-            )
             feed = _collection.feed(
                 title=f"{self.site_vars['SITE_TITLE']} - {_collection.title}",
                 link=self.site_vars["SITE_URL"],
@@ -115,9 +78,6 @@ class Site:
             feed.render(
                 path=self.path, engine=self.engine, **self.site_vars, **collection_vars
             )
-            p.update(create_feed, completed=1)
-
-        p.update(build_collection, advance=1)
 
         if _collection.has_archive:
             create_archives = p.add_task(
@@ -130,29 +90,13 @@ class Site:
                 archive=_collection.archives,
                 **self.site_vars,
             )
-            p.update(create_archives, completed=1)
-
-        p.update(build_collection, advance=1)
 
         if hasattr(_collection, "subcollections"):
-            created_subcollections = p.add_task(
-                f"[dark_goldenrod]- creating SubCollections {[x for x in _collection.subcollections]}!",
-                total=len(_collection.subcollections),
-            )
             for subcollection in _collection.subcollections:
-                created_subcollections_x = p.add_task(
-                    f"[misty_rose1]-- creating {subcollection.key} paths",
-                    total=1,
-                )
                 subcollection_path = collection_path / subcollection.key
                 subcollection_path.mkdir(exist_ok=True, parents=True)
-                p.update(created_subcollections_x, completed=1)
 
                 subgroups = _collection._gen_subpages(subcollection)
-                created_subcollections_x_subgroups = p.add_task(
-                    f"[light_goldenrod2]-- creating {subcollection.key} archives",
-                    total=len(subgroups),
-                )
 
                 for subgroup in subgroups:
                     render_archives(
@@ -162,18 +106,13 @@ class Site:
                         **self.site_vars,
                         **collection_vars,
                     )
-                    p.update(created_subcollections_x_subgroups, advance=1)
-                p.update(created_subcollections, advance=1)
 
-        p.update(build_collection, advance=1)
         return _collection
 
     def render_page(self, page) -> None:
         """Create a Page object and add it to self.routes"""
-        with p:
-
-            _page = page(**self.site_vars)
-            _page.render(path=self.path, engine=self.engine, **page.__dict__)
+        _page = page(**self.site_vars)
+        _page.render(path=self.path, engine=self.engine, **page.__dict__)
 
     def render_static(self, directory) -> None:
         """Copies a Static Directory to the output folder"""
