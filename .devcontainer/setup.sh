c6bea71484a111f704ac0b79fe380f40cd788079
Jay Miller <kjaymiller@gmail.com>
2022-12-17 15:59:21 -0800

    add twine to devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,5 @@
 python -m pip install --upgrade pip
+python -m pip install pip-tools
+python -m piptools compile -o requirements.txt pyproject.toml
 python -m pip install --user -r requirements.txt
 pre-commit install
