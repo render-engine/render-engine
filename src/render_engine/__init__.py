2beae69bca19fd2f784562d8d373d054aabe99c6
Jay Miller <kjaymiller@gmail.com>
2022-12-17 20:02:57 -0800

    update docs

--- src/render_engine/__init__.py
+++ src/render_engine/__init__.py
@@ -1,4 +1,7 @@
 from .blog import Blog
 from .collection import Collection
 from .page import Page
-from .site import Site
+
+# TODO: #70 site is a reserved keyword in python.
+# It should possibly become a new module name
+from .site import Site 
