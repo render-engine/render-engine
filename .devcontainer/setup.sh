9bc9dc603197393de85b67dd3d19405c3d930e32
Jay Miller <kjaymiller@gmail.com>
2023-03-01 19:41:08 +0000

    update devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,3 @@
-python -m pip install --upgrade pip
-python -m pip install pip-tools
-python -m piptools compile -o requirements.txt pyproject.toml
+python -m pip install --user --upgrade pip pip-tools
+python -m piptools compile --extra=dev -o requirements.txt pyproject.toml
 python -m pip install --user -r requirements.txt -r docs/requirements.txt
-python -m pip install --user -e .
-pre-commit install
