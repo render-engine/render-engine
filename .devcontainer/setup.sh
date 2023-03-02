cf5a7376cd96d99490ecf5a8d4a9ed1629a2c588
Jay Miller <kjaymiller@gmail.com>
2023-03-01 20:36:13 -0800

    update pip-tools reference and 3.11

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 python -m pip install --user --upgrade pip
-python -m pip install --user piptools
+python -m pip install --user pip-tools
 python -m piptools compile --upgrade --extra=dev -o requirements.txt pyproject.toml
 python -m pip install --user -r requirements.txt
