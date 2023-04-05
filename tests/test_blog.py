40859e9c68980a29574274f1ccb14b73b1338dd8
Jay Miller <kjaymiller@gmail.com>
2023-04-04 22:35:08 -0700

    creates _check_date_values()
    Took the code used to check `date_modified` and `date_published` and
    created a function to check the values.

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,8 +1,57 @@
+import datetime
 import pytest
 
-from render_engine.blog import Blog
+from render_engine.blog import (
+    Blog,
+    BlogPost,
+    _check_date_values,
+)
 
 
 def test_blog_has_feed():
     blog = Blog()
     assert hasattr(blog, "Feed")
+
+@pytest.mark.parametrize(
+    "date_key",
+    [
+        ("date"),
+        ("_date_published"),
+        ("publish_date"),
+    ]
+ )
+def test_blog_date_published_found_in_expected_parameters(date_key: str):
+    class CustomBlogPost(BlogPost):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            setattr(self, date_key, "2020-01-01")
+
+    blog = CustomBlogPost()
+
+    assert blog.date_published == datetime.datetime(2020, 1, 1,0,0,0)
+
+
+def test__check_date_values_raises_error_if_no_date_value_found():
+    with pytest.raises(ValueError):
+        _check_date_values("test_date_value", (None, None, None))
+        
+@pytest.mark.parametrize(
+    "date_attr",
+    [
+        ("date_modified"),
+        ("date_published"),
+    ]
+)
+def test_blog_post_datetime_parses_common_US_formats(date_attr: str):
+    """
+    Test that the date_published matches the expected common dates.
+    # TODO: test for other localizations
+    """
+
+    class CustomBlogPost(BlogPost):
+        def __init__(self):
+            super().__init__()
+            setattr(self, f"_{date_attr}", "2020-01-01")
+        
+    blog = CustomBlogPost()
+    assert getattr(blog, date_attr, datetime.date(2020,1,1))
