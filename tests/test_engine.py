ddc6c66fda75c10747973b9d33d73e2835f0821b
Jay Miller <kjaymiller@gmail.com>
2023-09-27 16:43:31 -0400

    adds test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,9 +6,10 @@ from render_engine.engine import format_datetime
 
 @pytest.mark.parametrize(
     "format, override, expected",
-    [{"DATETIME_FORMAT": "%B %d %Y %H:%M"}, None, "September 27 2023 14:00"], # Test given value in jinja global
-    [{}, None, "27 Sep 2023 14:00 UTC"], # Test default if no global set
-    [{"DATETIME_FORMAT": "%B %d %Y %H:%M"}, "%B %d %Y", "September 27 2023"], # Test override
+    (
+        ({"DATETIME_FORMAT": "%B %d %Y %H:%M"}, None, "September 27 2023 14:00"), # Test given value in jinja global
+        ({}, None, "27 Sep 2023 14:00 UTC"), # Test default if no global set
+        ({"DATETIME_FORMAT": "%B %d %Y %H:%M"}, "%B %d %Y", "September 27 2023"), # Test override
     )
 )
 def test_format_datetime(format, override, expected):
@@ -19,7 +20,7 @@ def test_format_datetime(format, override, expected):
     env = jinja2.Environment()
     env.globals.update(format)
     format_datetime(
-        env,
-        datetime.datetime(2023, 9, 27, 14, 0, 0),
-        override=override,
+        env=env,
+        value=datetime.datetime(2023, 9, 27, 14, 0, 0),
+        datetime_format=override,
     )
