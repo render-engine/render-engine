60b7ec192b36cd01f748a5190e3c8c2ff22a1248
Jay Miller <kjaymiller@gmail.com>
2023-12-19 15:09:24 -0500

    Changes-to-themes-and-plugins (#467)
    * updates site and themes
    
    * update plugins register plugins
    
    * updates test.yaml
    
    * moves parsers to packages
    
    * ruff linting on pluggy
    
    * adds logging messages for registering plugins and settings
    
    * more verbose choicing around theme settings
    
    * linting for site map extras
    
    * removes call for src in pyproject.toml
    
    * Format and lint with Ruff
    
    ---------
    
    Co-authored-by: Ruff <ruff-action@users.noreply.github.com>

--- src/render_engine/extras/site_map.py
+++ src/render_engine/extras/site_map.py
@@ -28,14 +28,18 @@ class SiteMap:
                 output_path: The path to the sitemap.xml file. output_path is relative to the output_path of the site
                 map_item_pattern: The pattern to use to find the files to include in the sitemap.xml file
         """
-        logging.info(
-            f"""Generating sitemap - {site.plugin_settings["SiteMap"]['output_path']}
-                     from files matching - {site.plugin_settings["SiteMap"]['map_item_pattern']}
-                     using template - {site.plugin_settings["SiteMap"]['template']}"""
+        logging.debug(
+            f"""Generating sitemap - {site.plugin_manager.plugin_settings["SiteMap"]['output_path']}
+                from files matching - {site.plugin_manager.plugin_settings["SiteMap"]['map_item_pattern']}
+                using template - {site.plugin_manager.plugin_settings["SiteMap"]['template']}"""
         )
-        template = engine.get_template(site.plugin_settings["SiteMap"]["template"])
-        site_map_items = pathlib.Path(site.output_path).rglob(site.plugin_settings["SiteMap"]["map_item_pattern"])
-        sitemap_path = pathlib.Path(site.output_path).joinpath(site.plugin_settings["SiteMap"]["output_path"])
+        template = engine.get_template(site.plugin_manager.plugin_settings["SiteMap"]["template"])
+        site_map_items = pathlib.Path(site.output_path).rglob(
+            site.plugin_manager.plugin_settings["SiteMap"]["map_item_pattern"]
+        )
+        sitemap_path = pathlib.Path(site.output_path).joinpath(
+            site.plugin_manager.plugin_settings["SiteMap"]["output_path"]
+        )
         sitemap_path.write_text(
             template.render(
                 items=[item.relative_to(site.output_path) for item in site_map_items],
