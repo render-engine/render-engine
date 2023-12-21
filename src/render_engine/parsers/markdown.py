e3fe83e4af907b75d7e215256a0465f71d394ff8
kjaymiller <kjaymiller@gmail.com>
2023-12-21 00:04:40 -0500

    lint

--- src/render_engine/parsers/markdown.py
+++ src/render_engine/parsers/markdown.py
@@ -3,6 +3,7 @@ import warnings
 from render_engine_markdown import MarkdownPageParser  # noqa: F401
 
 warnings.warn(
-    "`render_engine.hookspecs` will be deprecated in version 2024.3.1,  Please use `from render_engine_markdown import MarkdownPageParser` instead.",
+    "`render_engine.hookspecs` will be deprecated in version 2024.3.1,  \
+        Please use `from render_engine_markdown import MarkdownPageParser` instead.",
     DeprecationWarning,
 )
