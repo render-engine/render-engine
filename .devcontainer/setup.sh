80c06c1116047b9cbf66eac95f9d569ae25c9a0a
kjaymiller <kjaymiller@gmail.com>
2024-02-01 18:35:42 -0500

    remove pinning from test and pytest-lazy-fixture

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,2 @@
 python -m pip install --user --upgrade pip
-python -m pip install --user pip-tools
-python -m piptools compile --upgrade --extra=dev -o requirements.txt pyproject.toml
-python -m pip install --user -r requirements.txt
+python -m pip install .
