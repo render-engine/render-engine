7d246e08049f1632301a36e1ad12c6bbe67a5fb4
kjaymiller <kjaymiller@gmail.com>
2024-02-12 13:15:12 -0500

    update documentation and lint tests
    udpates the documentation around the theme management.
    
    - corrects how to add static paths

--- src/render_engine/site.py
+++ src/render_engine/site.py
@@ -23,7 +23,7 @@ class Site:
         partial:
             if True, only render pages that have been modified. Uses gitPython to check for changes.
         static_paths:
-            list of paths for static folders. This will get copied to the output folder. Folders are recursive.
+            set of paths for static folders. This will get copied to the output folder. Folders are recursive.
         site_vars:
             dictionary that will be passed into page template
         site_settings:
@@ -56,7 +56,9 @@ class Site:
         self.site_settings = dict()
         self.subcollections = defaultdict(lambda: {"pages": []})
         self.theme_manager.engine.globals.update(self.site_vars)
-        self.theme_manager.engine.loader.loaders.insert(0, FileSystemLoader(self._template_path))
+        self.theme_manager.engine.loader.loaders.insert(
+            0, FileSystemLoader(self._template_path)
+        )
 
     @property
     def output_path(self) -> str:
@@ -74,7 +76,9 @@ class Site:
         for plugin in plugins:
             logging.debug("Registering Plugin: %s", plugin.__name__)
             self.plugin_manager.register_plugin(plugin)
-            logging.debug("Loading default settings: %s", getattr(plugin, "default_settings", {}))
+            logging.debug(
+                "Loading default settings: %s", getattr(plugin, "default_settings", {})
+            )
             self.plugin_manager.plugin_settings[plugin.__name__] = {
                 **getattr(plugin, "default_settings", {}),
                 **getattr(self.plugin_settings, plugin.__name__, {}),
@@ -182,17 +186,25 @@ class Site:
 
     def _render_output(self, route: str, page: Page) -> int:
         """writes the page object to disk"""
-        path = pathlib.Path(self.output_path) / pathlib.Path(route) / pathlib.Path(page.path_name)
+        path = (
+            pathlib.Path(self.output_path)
+            / pathlib.Path(route)
+            / pathlib.Path(page.path_name)
+        )
         path.parent.mkdir(parents=True, exist_ok=True)
         settings = {**self.site_settings.get("plugins", {}), **{"route": route}}
 
         if hasattr(page, "plugin_manager"):
-            page.plugin_manager._pm.hook.render_content(page=page, settings=settings, site=self)
+            page.plugin_manager._pm.hook.render_content(
+                page=page, settings=settings, site=self
+            )
         page.rendered_content = page._render_content(engine=self.theme_manager.engine)
         # pass the route to the plugin settings
 
         if hasattr(page, "plugin_manager"):
-            page.plugin_manager._pm.hook.post_render_content(page=page.__class__, settings=settings, site=self)
+            page.plugin_manager._pm.hook.post_render_content(
+                page=page.__class__, settings=settings, site=self
+            )
 
         return path.write_text(page.rendered_content)
 
@@ -247,7 +259,9 @@ class Site:
             logging.info(f"loading theme: {theme_prefix}")
             self.theme_manager.engine.loader.loaders.insert(-1, theme_loader)
         # load themes in the PrefixLoader
-        self.theme_manager.engine.loader.loaders.insert(-1, PrefixLoader(self.theme_manager.prefix))
+        self.theme_manager.engine.loader.loaders.insert(
+            -1, PrefixLoader(self.theme_manager.prefix)
+        )
 
     @property
     def template_path(self) -> str:
@@ -255,7 +269,9 @@ class Site:
 
     @template_path.setter
     def template_path(self, template_path: str) -> None:
-        self.theme_manager.engine.loader.loaders.insert(0, FileSystemLoader(template_path))
+        self.theme_manager.engine.loader.loaders.insert(
+            0, FileSystemLoader(template_path)
+        )
 
     def render(self) -> None:
         """
@@ -278,7 +294,9 @@ class Site:
             self.load_themes()
             self.theme_manager.engine.globals.update(self.site_vars)
             # Parse Route List
-            task_add_route = progress.add_task("[blue]Adding Routes", total=len(self.route_list))
+            task_add_route = progress.add_task(
+                "[blue]Adding Routes", total=len(self.route_list)
+            )
 
             self.theme_manager._render_static()
 
@@ -286,7 +304,9 @@ class Site:
             self.theme_manager.engine.globals["routes"] = self.route_list
 
             for slug, entry in self.route_list.items():
-                progress.update(task_add_route, description=f"[blue]Adding[gold]Route: [blue]{slug}")
+                progress.update(
+                    task_add_route, description=f"[blue]Adding[gold]Route: [blue]{slug}"
+                )
                 if isinstance(entry, Page):
                     for route in entry.routes:
                         progress.update(
