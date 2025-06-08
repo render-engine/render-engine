8d78e0cfa848e0523bad8b30889ebf98b165bc03
Jay Miller <kjaymiller@gmail.com>
2025-06-08 14:55:58 -0400

    use datetime timeznoe utc and adds support to 3.10 (#940)
    * fix datetime to make testing compatible with 3.10
    
    * add 3.10 to ci/cd tests

--- tests/test_feeds/test_feeds.py
+++ tests/test_feeds/test_feeds.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import timezone
 
 import pluggy
 import pytest
@@ -78,7 +79,7 @@ def test_rss_feed_template_with_strictundefined(engine, tmp_path):
 @pytest.mark.parametrize(
     "date",
     [
-        datetime.datetime(2023, 4, 15, 0, 0, 0, tzinfo=datetime.UTC),
+        datetime.datetime(2023, 4, 15, 0, 0, 0, tzinfo=timezone.utc),
         datetime.datetime(2023, 4, 15, 0, 0, 0, tzinfo=None),
         datetime.date(2024, 4, 15),
     ],
@@ -87,7 +88,7 @@ def test_rss_feed_template_parses_date_correctly(engine, date):
     """Tests that a feed parses the page date in RFC2822 Format"""
 
     class TestPage(Page):
-        date = datetime.datetime(2023, 4, 15, 0, 0, 0, tzinfo=datetime.UTC)  # noqa: UP017
+        date = datetime.datetime(2023, 4, 15, 0, 0, 0, tzinfo=timezone.utc)  # noqa: UP017
 
     class TestCollection(Collection):
         Feed = RSSFeed
