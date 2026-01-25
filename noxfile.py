e87a29d0ec4ebe285b3d1236fe16a11cea55179c
Jay Miller <kjaymiller@gmail.com>
2026-01-25 18:32:05 -0500

    remove linting from noxfile (#1047)
    * remove linting from noxfile
    
    * update formatting

--- noxfile.py
+++ noxfile.py
@@ -3,15 +3,6 @@ import nox
 PYTHON_VERSIONS = ["3.14", "3.13", "3.12", "3.11", "3.10"]
 
 
-@nox.session
-def lint(session, python="3.13"):
-    """Lint using ruff"""
-
-    session.install("ruff")
-    session.run("ruff", "check", "--fix", ".")
-    session.run("ruff", "format", ".")
-
-
 @nox.session(python=PYTHON_VERSIONS)
 def test(session):
     """Run the test suite"""
