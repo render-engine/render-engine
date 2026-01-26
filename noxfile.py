bcf00f388ecfb4c7beb63306c07e0d8d0956a10f
Jay Miller <kjaymiller@gmail.com>
2026-01-25 19:11:45 -0500

    add uv to nox

--- noxfile.py
+++ noxfile.py
@@ -2,10 +2,17 @@ import nox
 
 PYTHON_VERSIONS = ["3.14", "3.13", "3.12", "3.11", "3.10"]
 
-
-@nox.session(python=PYTHON_VERSIONS)
-def test(session):
+@nox.session(python=PYTHON_VERSIONS, venv_backend="uv")
+def test(session: nox.Session) -> None:
     """Run the test suite"""
-    session.install("pytest")
-    session.install(".[dev]")
+    session.run_install(
+            "uv",
+            "sync",
+            "--extra",
+            "dev",
+            "--quiet",
+            f"--python={session.virtualenv.location}",
+            env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+            external=True,
+    )
     session.run("pytest", "tests")
